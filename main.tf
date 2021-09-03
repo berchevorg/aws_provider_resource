8998b2aace9b0fb3796405c4a3389945857ff7e4
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-09-03 07:21:31 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Owner = "Georgiman"
+    }
+  }
+}
+
+resource "aws_vpc" "georgiman_vpc" {
+  cidr_block           = "16.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
