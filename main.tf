49263fbb591ffae32ca02bf5bc000d0316f441aa
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-03-11 09:33:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.74.3"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
-  
   default_tags {
     tags = {
       Owner = "Georgiman"
